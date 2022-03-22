0f7d6372dbfb8c0bfe4461cf77bcba97014fd4d8
Mehdi Samsami <mehdisamsami@live.com>
2022-03-22 17:15:10 +0430

    Optimize __calculate_grad method Fix a bug in predict method Add __prepare_parameters and __prepare_X_y methods Add function typehints

--- wnb/WeightedNB.py
+++ wnb/WeightedNB.py
@@ -1,13 +1,19 @@
+import time
+
 import numpy as np
 import pandas as pd
 from scipy.stats import norm
+from typing import Union
 
 
 class WeightedNB:
 
-    def __init__(self, priors=None, error_weights=None, max_iter=25, step_size=1e-4, penalty='l2', C=1.0):
+    def __init__(self, priors: Union[list, np.ndarray, None] = None, error_weights: Union[np.ndarray, None] = None,
+                 max_iter: int = 25, step_size: float = 1e-4, penalty: str = 'l2', C: float = 1.0):
         self.priors = priors  # Prior probabilities of classes (n_classes x 1)
+        self.__priors = None
         self.error_weights = error_weights  # Matrix of error weights (n_features x n_features)
+        self.__error_weights = None
         self.max_iter = max_iter  # Maximum number of iterations of the learning algorithm
         self.step_size = step_size  # Learning rate
         self.penalty = penalty  # Regularization type ('l1' or 'l2')
@@ -30,11 +36,6 @@ class WeightedNB:
         if self.n_classes != 2:
             raise ValueError('This version of MLD-WNB only supports binary classification.')
 
-        # Check that the number of samples and labels are compatible
-        if self.n_samples != y.shape[0]:
-            msg = "X.shape[0]=%d and y.shape[0]=%d are incompatible."
-            raise ValueError(msg % (X.shape[0], y.shape[0]))
-
         if self.priors is not None:
             # Check that the provided priors match the number of classes
             if len(self.priors) != self.n_classes:
@@ -49,7 +50,10 @@ class WeightedNB:
         if self.error_weights is not None:
             # Check that the size of error weights matrix matches number of classes
             if self.error_weights.shape != (self.n_classes, self.n_classes):
-                raise ValueError('The size of error weights matrix does not match the number of classes.')
+                raise ValueError(
+                    'The shape of error weights matrix does not match the number of classes, '
+                    'must be (n_classes, n_classes).'
+                )
 
         # Check that the maximum number of iterations is a positive integer
         if type(self.max_iter) is not int or self.max_iter < 1:
@@ -59,18 +63,7 @@ class WeightedNB:
         if self.penalty not in ['l1', 'l2']:
             raise ValueError("Regularization type must be either 'l1' or 'l2'.")
 
-    def fit(self, X, y, learning_hist=False):
-        # Convert to NumPy array if X and/or y are Pandas DataFrame
-        if isinstance(X, pd.DataFrame):
-            X = X.values
-        if isinstance(y, pd.DataFrame):
-            y = y.values.flatten()
-
-        self.classes_, self.class_count_ = np.unique(y, return_counts=True)  # Unique class labels and their counts
-        self.n_classes = len(self.classes_)  # Find the number of classes
-        self.n_samples, self.n_features = X.shape  # Find the number of samples and features
-
-        self.__check_inputs(X, y)
+    def __prepare_parameters(self, X, y):
 
         # Calculate mean and standard deviation of features for each class
         self.mu = np.zeros((self.n_features, self.n_classes))
@@ -81,14 +74,54 @@ class WeightedNB:
 
         # Update if no priors is provided
         if self.priors is None:
-            self.priors = self.class_count_ / self.n_samples  # Calculate empirical prior probabilities
+            self.__priors = self.class_count_ / self.n_samples  # Calculate empirical prior probabilities
+        else:
+            self.__priors = self.priors
+
         # Convert to NumPy array in input priors is in a list
-        if type(self.priors) is list:
-            self.priors = np.array(self.priors)
+        if type(self.__priors) is list:
+            self.__priors = np.array(self.__priors)
 
         # Update if no error weights is provided
         if self.error_weights is None:
-            self.error_weights = np.array([[0, 1], [-1, 0]])
+            self.__error_weights = np.array([[0, 1], [-1, 0]])
+        else:
+            self.__error_weights = self.error_weights
+
+    def __prepare_X_y(self, X=None, y=None):
+        if X is not None:
+            # Convert to NumPy array if X is Pandas DataFrame
+            if isinstance(X, pd.DataFrame):
+                X = X.values
+
+        if y is not None:
+            # Convert to a flat NumPy array
+            if isinstance(y, pd.DataFrame) or isinstance(y, pd.Series):
+                y = y.values
+            y = y.flatten()
+
+        output = tuple(item for item in [X, y] if item is not None)
+        output = output[0] if len(output) == 1 else output
+        return output
+
+    def fit(self, X: Union[np.ndarray, pd.DataFrame], y: Union[np.ndarray, pd.DataFrame, pd.Series],
+            learning_hist: bool = False):
+
+        X, y = self.__prepare_X_y(X, y)
+
+        self.classes_, self.class_count_ = np.unique(y, return_counts=True)  # Unique class labels and their counts
+        self.n_classes = len(self.classes_)  # Find the number of classes
+        self.n_samples, self.n_features = X.shape  # Find the number of samples and features
+
+        # Check that the number of samples and labels are compatible
+        if self.n_samples != y.shape[0]:
+            raise ValueError(
+                "X.shape[0]=%d and y.shape[0]=%d are incompatible." % (X.shape[0], y.shape[0])
+            )
+
+        self.__check_inputs(X, y)
+
+        self.__prepare_parameters(X, y)
 
         self.weights_ = np.ones((self.n_features,))  # Initialize the weights
         self.cost_hist_ = np.zeros((self.max_iter,))  # To store history of cost changes
@@ -101,14 +134,15 @@ class WeightedNB:
             # Calculate cost
             self.cost_hist_[_iter], _lambda = self.__calculate_cost(X, y, y_hat, learning_hist)
 
-            # Calculate gradients
+            # Calculate gradients (most time-consuming)
             _grad = self.__calculate_grad(X, _lambda)
 
             # Add regularization
-            if self.penalty == 'l1':
-                _grad += self.C * np.sign(self.weights_)
-            elif self.penalty == 'l2':
-                _grad += 2 * self.C * self.weights_
+            _grad += (self.C * np.sign(self.weights_)) if self.penalty == 'l1' else (2 * self.C * self.weights_)
+            # if self.penalty == 'l1':
+            #     _grad += self.C * np.sign(self.weights_)
+            # elif self.penalty == 'l2':
+            #     _grad += 2 * self.C * self.weights_
 
             # Update weights
             self.weights_ = self.weights_ - self.step_size * _grad
@@ -116,54 +150,64 @@ class WeightedNB:
         self._fit_status = True
 
     def __calculate_cost(self, X, y, y_hat, learning_hist):
-        _lambda = [self.error_weights[y[i], y_hat[i]] for i in range(self.n_samples)]
+        _lambda = [self.__error_weights[y[i], y_hat[i]] for i in range(self.n_samples)]
 
         if learning_hist:
             # Calculate cost
             _cost = 0
             for i in range(self.n_samples):
-                _sum = np.log(self.priors[1] / self.priors[0])
+                _sum = np.log(self.__priors[1] / self.__priors[0])
                 x = X[i, :]
                 for j in range(self.n_features):
                     _sum += self.weights_[j] * (np.log(1e-20 + norm.pdf(x[j], self.mu[j, 1], self.std[j, 1]))
                                                 - np.log(1e-20 + norm.pdf(x[j], self.mu[j, 0], self.std[j, 0])))
                 _cost += _lambda[i] * _sum
         else:
-            _cost = 0
+            _cost = None
 
         return _cost, _lambda
 
     def __calculate_grad(self, X, _lambda):
-        _grad = np.zeros((self.n_features,))
+        # _grad = np.zeros((self.n_features,))
+        # for i in range(self.n_samples):
+        #     x = X[i, :]
+        #     _log_p = np.array(
+        #         [
+        #             np.log(self.std[j, 0] / self.std[j, 1]) +
+        #             0.5*((x[j] - self.mu[j, 0]) / self.std[j, 0])**2 -
+        #             0.5*((x[j] - self.mu[j, 1]) / self.std[j, 1])**2
+        #             for j in range(self.n_features)
+        #         ]
+        #     )
+        #     _grad += _lambda[i] * _log_p
 
-        for i in range(self.n_samples):
-            x = X[i, :]
-            _log_p = np.array([np.log(self.std[j, 0] / self.std[j, 1]) +
-                               0.5*((x[j] - self.mu[j, 0]) / self.std[j, 0])**2 -
-                               0.5*((x[j] - self.mu[j, 1]) / self.std[j, 1])**2
-                               for j in range(self.n_features)])
-            _grad += _lambda[i] * _log_p
+        _grad = np.repeat(np.log(self.std[:, 0] / self.std[:, 1]).reshape(1, -1), self.n_samples, axis=0)
+        _grad += 0.5 * ((X - np.repeat(self.mu[:, 0].reshape(1, -1), self.n_samples, axis=0)) /
+                        (np.repeat(self.std[:, 0].reshape(1, -1), self.n_samples, axis=0))) ** 2
+        _grad -= 0.5 * ((X - np.repeat(self.mu[:, 1].reshape(1, -1), self.n_samples, axis=0)) /
+                        (np.repeat(self.std[:, 1].reshape(1, -1), self.n_samples, axis=0))) ** 2
+        _grad *= np.transpose(np.repeat(np.array(_lambda).reshape(1, -1), self.n_features, axis=0))
+        _grad = np.sum(_grad, axis=0)
 
         return _grad
 
-    def predict(self, X):
+    def predict(self, X: Union[np.ndarray, pd.DataFrame]):
         p_hat = self.predict_log_proba(X)
         y_hat = self.classes_[np.argmax(p_hat, axis=1)]
         return y_hat
 
-    def predict_log_proba(self, X):
+    def predict_log_proba(self, X: Union[np.ndarray, pd.DataFrame]):
         # if not self._fit_status:
         #     raise Exception('Model is not fitted.')
 
         if not X.shape[1] == self.n_features:
-            raise ValueError("Expected input with %d features, got %d instead."
-                             % (self.n_features, X.shape[1]))
+            raise ValueError(
+                "Expected input with %d features, got %d instead." % (self.n_features, X.shape[1])
+            )
 
-        # Convert to NumPy array if X is Pandas DataFrame
-        if isinstance(X, pd.DataFrame):
-            X = X.values
+        X = self.__prepare_X_y(X=X)
 
-        log_priors = np.tile(np.log(self.priors), (self.n_samples, 1))
+        log_priors = np.tile(np.log(self.__priors), (self.n_samples, 1))
         w_reshaped = np.tile(self.weights_.reshape(-1, 1), (1, self.n_classes))
         term1 = np.sum(np.multiply(w_reshaped, -np.log(np.sqrt(2 * np.pi) * self.std)))
         var_inv = np.multiply(w_reshaped, 1/np.multiply(self.std, self.std))
@@ -176,20 +220,19 @@ class WeightedNB:
 
     def predict_proba(self, X):
         log_proba = self.predict_log_proba(X)
+        proba = np.array([np.exp(row_log_proba) / (np.exp(row_log_proba)).sum() for row_log_proba in log_proba])
 
-        proba = np.zeros(log_proba.shape)
-        for i in range(1):
-            s_class = np.arange(1, self.n_classes + 1)
-            s_class = [c for c in s_class if c != i]
-            proba[:, i] = 1 / (1 + np.sum(np.exp(log_proba[:, s_class] - log_proba[:, i]), axis=1))
-        proba[:, -1] = 1 - np.sum(proba, axis=1)
+        # proba = np.zeros(log_proba.shape)
+        # for i in range(1):
+        #     s_class = np.arange(1, self.n_classes + 1)
+        #     s_class = [c for c in s_class if c != i]
+        #     proba[:, i] = 1 / (1 + np.sum(np.exp(log_proba[:, s_class] - log_proba[:, i]), axis=1))
+        # proba[:, -1] = 1 - np.sum(proba, axis=1)
 
         return proba
 
     def score(self, X, y):
         y_hat = self.predict(X)
-        if isinstance(y, pd.Series) or isinstance(y, pd.DataFrame):
-            y = y.values
-        y = y.flatten()
+        y = self.__prepare_X_y(y=y)
         _score = (y == y_hat).sum() / len(y)
         return _score
