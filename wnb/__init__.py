02150aa5bb26482bff3b8b23f98674466be42243
Mehdi Samsami <mehdisamsami@live.com>
2023-04-21 03:13:09 +0330

    update init, setup, and readme for the new version v0.1.0

--- wnb/__init__.py
+++ wnb/__init__.py
@@ -1,7 +1,12 @@
-__version__ = "0.0.5"
+__version__ = "0.1.0"
 __author__ = "Mehdi Samsami"
 
-__all__ = ['GaussianWNB', 'Distribution']
+__all__ = [
+    'GeneralNB',
+    'GaussianWNB',
+    'Distribution'
+]
 
 from ._enums import Distribution
+from .gnb import GeneralNB
 from .gwnb import GaussianWNB
