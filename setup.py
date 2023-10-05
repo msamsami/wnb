aa79231ce685c3c62e531cd4ff8da5e95d7051a1
Mehdi Samsami <mehdisamsami@live.com>
2023-10-05 20:38:57 +0330

    fix setup, add license and manifest (v0.1.17)

--- setup.py
+++ setup.py
@@ -5,17 +5,19 @@ from setuptools import setup, find_packages
 
 setup(
     name="wnb",
-    version="0.1.16",
+    version="0.1.17",
     description="Python library for the implementations of general and weighted naive Bayes (WNB) classifiers.",
     keywords=["python", "bayes", "naivebayes", "classifier", "probabilistic"],
     author="Mehdi Samsami",
     author_email="mehdisamsami@live.com",
+    license="BSD License",
     url="https://github.com/msamsami/weighted-naive-bayes",
     long_description=codecs.open(
         path.join(path.abspath(path.dirname(__file__)), "README.md"), encoding="utf-8"
     ).read(),
     long_description_content_type="text/markdown",
-    packages=find_packages(),
+    packages=find_packages(exclude=["tests*"]),
+    include_package_data=True,
     classifiers=[
         "Intended Audience :: Science/Research",
         "Intended Audience :: Developers",
@@ -28,7 +30,7 @@ setup(
         "Programming Language :: Python :: 3.9",
         "Programming Language :: Python :: 3.10",
         "Programming Language :: Python :: 3.11",
-        "License :: OSI Approved :: MIT License",
+        "License :: OSI Approved :: BSD License",
     ],
     python_requires=">=3.7",
     install_requires=["pandas==1.4.1", "scikit-learn>=1.0.2"],
