84ca052df6c5c362e24d4e8213ec0f2e643268ce
Mehdi Samsami <mehdisamsami@live.com>
2023-10-07 13:41:53 +0330

    rename tests, add pdf and out-of-support tests for exponential dist, decrease precision

--- tests/test_dist.py
+++ tests/test_dist.py
@@ -1,8 +1,8 @@
 import numpy as np
 
 import pytest
-from scipy.stats import norm, lognorm
-from sklearn.utils._testing import assert_array_equal, assert_array_almost_equal
+from scipy.stats import norm, lognorm, expon, uniform
+from sklearn.utils._testing import assert_array_almost_equal
 
 from wnb import Distribution as D
 from wnb.dist import (
@@ -44,54 +44,74 @@ def test_distributions_correct_support_attr():
                 assert isinstance(x, (float, int))
 
 
-def test_normal_dist_pdf():
+def test_normal_pdf():
     """
     Test whether pdf method of NormalDist returns the same result as pdf method of scipy.stats.norm.
     """
     norm_wnb = NormalDist(mu=1, sigma=3)
     norm_scipy = norm(loc=1, scale=3)
     X = np.random.uniform(-50, 50, size=10000)
-    assert_array_almost_equal(norm_wnb(X), norm_scipy.pdf(X), decimal=15)
+    assert_array_almost_equal(norm_wnb(X), norm_scipy.pdf(X), decimal=10)
 
 
-def test_lognormal_dist_pdf():
+def test_lognormal_pdf():
     """
     Test whether pdf method of LognormalDist returns the same result as pdf method of scipy.stats.lognorm.
     """
     lognorm_wnb = LognormalDist(mu=1, sigma=3)
     lognorm_scipy = lognorm(scale=np.exp(1), s=3)
     X = np.random.uniform(0, 50, size=10000)
-    assert_array_almost_equal(lognorm_wnb(X), lognorm_scipy.pdf(X), decimal=15)
+    assert_array_almost_equal(lognorm_wnb(X), lognorm_scipy.pdf(X), decimal=10)
 
 
-def test_lognormal_dist_out_of_support_data():
+def test_lognormal_out_of_support_data():
     """
     Test whether a warning is issued when calling LognormalDist with out-of-support data.
     """
     X = np.random.lognormal(mean=1.5, sigma=3.75, size=1000)
-    lognormal = LognormalDist.from_data(X)
+    lognorm_wnb = LognormalDist.from_data(X)
 
     with pytest.warns(RuntimeWarning, match=out_of_support_warn_msg):
-        lognormal(-1)
+        lognorm_wnb(-1)
 
 
-def test_exponential_dist_out_of_support_data():
+def test_exponential_pdf():
+    """
+    Test whether pdf method of ExponentialDist returns the same result as pdf method of scipy.stats.expon.
+    """
+    expon_wnb = ExponentialDist(rate=4)
+    expon_scipy = expon(scale=1 / 4)
+    X = np.random.uniform(0, 100, size=10000)
+    assert_array_almost_equal(expon_wnb(X), expon_scipy.pdf(X), decimal=10)
+
+
+def test_exponential_out_of_support_data():
     """
     Test whether a warning is issued when calling ExponentialDist with out-of-support data.
     """
     X = np.random.exponential(scale=2.5, size=1000)
-    exp = ExponentialDist.from_data(X)
+    expon_wnb = ExponentialDist.from_data(X)
 
     with pytest.warns(RuntimeWarning, match=out_of_support_warn_msg):
-        exp(-1)
+        expon_wnb(-1)
 
 
-def test_exponential_dist_out_of_support_data():
+def test_uniform_pdf():
+    """
+    Test whether pdf method of UniformDist returns the same result as pdf method of scipy.stats.uniform.
+    """
+    uniform_wnb = UniformDist(a=-5, b=10)
+    uniform_scipy = uniform(loc=-5, scale=15)
+    X = np.random.uniform(0, 100, size=10000)
+    assert_array_almost_equal(uniform_wnb(X), uniform_scipy.pdf(X), decimal=10)
+
+
+def test_uniform_out_of_support_data():
     """
     Test whether a warning is issued when calling UniformDist with out-of-support data.
     """
-    X = np.random.uniform(low=5, high=10, size=10000)
-    uniform = UniformDist.from_data(X)
+    X = np.random.uniform(low=5, high=10, size=1000)
+    uniform_wnb = UniformDist.from_data(X)
 
     with pytest.warns(RuntimeWarning, match=out_of_support_warn_msg):
-        uniform(3)
+        uniform_wnb(3)
