67c20ace0e1bd76d6bd6d64a7ccd1ea57b76e337
Mehdi Samsami <mehdisamsami@live.com>
2023-10-09 16:07:05 +0330

    add a example for using general naive bayes classifier

---
+++ examples/gnb_wine.py
@@ -0,0 +1,26 @@
+from sklearn.datasets import load_wine
+from sklearn.model_selection import train_test_split
+from sklearn.naive_bayes import GaussianNB
+
+from wnb import GeneralNB, Distribution as D
+
+
+# Load the breast cancer wisconsin dataset
+wine = load_wine()
+X = wine["data"]
+y = wine["target"]
+
+# Split the data into training and test sets
+X_train, X_test, y_train, y_test = train_test_split(
+    X, y, test_size=0.33, random_state=0
+)
+
+# Train and score sklearn GaussianNB classifier
+gnb = GaussianNB()
+gnb.fit(X_train, y_train)
+print("sklearn | GaussianNB >> score >>", gnb.score(X_test, y_test))
+
+# Train and score wnb GeneralNB classifier
+gnb = GeneralNB(distributions=[D.LOGNORMAL] * X.shape[1])
+gnb.fit(X_train, y_train)
+print("wnb | GeneralNB >> score >>", gnb.score(X_test, y_test))
