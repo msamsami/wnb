be243830038489cb0583c2655ff7bee39b5793a8
Mehdi Samsami <mehdisamsami@live.com>
2023-12-29 21:49:59 +0330

    redefine types

--- wnb/_typing.py
+++ wnb/_typing.py
@@ -1,12 +1,20 @@
 from typing import Union, Type
 
-from sklearn._typing import MatrixLike, ArrayLike, Float
+import numpy.typing
+import numpy as np
+import pandas as pd
+from scipy.sparse import spmatrix
 
 from ._base import ContinuousDistMixin, DiscreteDistMixin
 from ._enums import Distribution
 
-__all__ = ["MatrixLike", "ArrayLike", "Float", "DistibutionLike"]
+__all__ = ["MatrixLike", "ArrayLike", "Int", "Float", "DistibutionLike"]
 
+ArrayLike = numpy.typing.ArrayLike
+MatrixLike = Union[np.ndarray, pd.DataFrame, spmatrix]
+
+Int = Union[int, np.int8, np.int16, np.int32, np.int64]
+Float = Union[float, np.float16, np.float32, np.float64]
 
 DistibutionLike = Union[
     str,
