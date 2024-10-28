309c42b4524b76e7555d48db2f02da3d63e13765
Mehdi Samsami <mehdisamsami@live.com>
2024-10-28 22:43:40 +0330

    improve type hints, minor changes

--- tests/benchmarks/utils.py
+++ tests/benchmarks/utils.py
@@ -9,7 +9,7 @@ from sklearn.model_selection import train_test_split
 
 try:
     from tqdm import tqdm
-except Exception:
+except ModuleNotFoundError:
     tqdm = lambda iterable, *args, **kwargs: iterable
 
 warnings.filterwarnings("ignore")
@@ -17,22 +17,21 @@ warnings.filterwarnings("ignore")
 __all__ = ("benchmark",)
 
 
-def compare_score(X, y, wnb, sklearn, random_state: int, test_size: float) -> tuple[float, float]:
-    # Split the data into training and test sets
+def compare_score(X, y, wnb, sklearn, random_state: int, test_size: float) -> tuple[np.float64, np.float64]:
     X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=test_size, random_state=random_state)
 
-    # Train and score wnb classifier
     clf_wnb = clone(wnb)
     clf_wnb.fit(X_train, y_train)
 
-    # Train and score sklearn classifier
     clf_sklearn = clone(sklearn)
     clf_sklearn.fit(X_train, y_train)
 
     return clf_wnb.score(X_test, y_test), clf_sklearn.score(X_test, y_test)
 
 
-def benchmark(X, y, wnb, sklearn, max_iter: int = 50, test_size: float = 0.33) -> tuple[float, float]:
+def benchmark(
+    X, y, wnb, sklearn, max_iter: int = 50, test_size: float = 0.33
+) -> tuple[np.float64, np.float64]:
     results = Parallel(n_jobs=-1, prefer="processes")(
         delayed(compare_score)(*param)
         for param in tqdm([(X, y, wnb, sklearn, i, test_size) for i in range(max_iter)], ncols=80)
