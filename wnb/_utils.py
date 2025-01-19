ac9302f905c6e8f62414642823c1749908ac44db
Mehdi Samsami <mehdisamsami@live.com>
2025-01-19 04:32:02 +0330

    major internal refactor in general naive bayes class

--- wnb/_utils.py
+++ wnb/_utils.py
@@ -7,6 +7,7 @@ from sklearn.utils import check_array
 __all__ = [
     "SKLEARN_V1_6_OR_LATER",
     "validate_data",
+    "check_X_y",
     "_check_n_features",
     "_check_feature_names",
 ]
@@ -17,6 +18,7 @@ SKLEARN_V1_6_OR_LATER = version.parse(sklearn.__version__) >= version.parse("1.6
 
 if SKLEARN_V1_6_OR_LATER:
     from sklearn.utils.validation import _check_feature_names, _check_n_features
+    from sklearn.utils.validation import check_X_y as _check_X_y
     from sklearn.utils.validation import validate_data as _validate_data
 
     def validate_data(*args, **kwargs):
@@ -24,11 +26,19 @@ if SKLEARN_V1_6_OR_LATER:
             kwargs["ensure_all_finite"] = kwargs.pop("force_all_finite")
         return _validate_data(*args, **kwargs)
 
+    def check_X_y(*args, **kwargs):
+        if kwargs.get("force_all_finite"):
+            kwargs["ensure_all_finite"] = kwargs.pop("force_all_finite")
+        return _check_X_y(*args, **kwargs)
+
 else:
 
     def validate_data(estimator, X, **kwargs: Any):
         kwargs.pop("reset", None)
         return check_array(X, estimator=estimator, **kwargs)
+
+    def check_X_y(*args, **kwargs):
+        return _check_X_y(*args, **kwargs)
 
     def _check_n_features(estimator, X, reset):
         return estimator._check_n_features(X, reset=reset)
